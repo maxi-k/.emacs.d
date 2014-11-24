43b2a949dc8c444d92bb56a15ee4fb0831f06f98
maxi-k <maxi.kuschewski@gmail.com>
2014-11-24 14:35:44 +0100

    Different indentation mode for haskell

--- user/mode-settings/haskell-settings.el
+++ user/mode-settings/haskell-settings.el
@@ -1,7 +1,7 @@
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc)
-;; (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 ;; (add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
-(add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
+;; (add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
 
 (add-hook 'haskell-mode-hook 'inf-haskell-mode)
 
