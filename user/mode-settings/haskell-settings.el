23f305b490aa5a072d12ef443ba2bb3ccc45a80b
maxi-k <maxi.kuschewski@gmail.com>
2014-12-23 19:19:45 +0100

    Clean-up

--- user/mode-settings/haskell-settings.el
+++ user/mode-settings/haskell-settings.el
@@ -1,11 +1,10 @@
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc)
+
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 ;; (add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
 ;; (add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
 
 (add-hook 'haskell-mode-hook 'inf-haskell-mode)
-
-
 (add-hook 'haskell-mode-hook 'flycheck-mode)
 
 (provide 'haskell-settings)
