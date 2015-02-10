604a9180aa7dd1bf69d78edc682c2dc7e4860ed7
maxi-k <maxi.kuschewski@gmail.com>
2015-02-10 23:34:12 +0100

    Changed initial frame size

--- user/look.el
+++ user/look.el
@@ -51,7 +51,7 @@
 
 ;; Set the initial window size
 (setq initial-frame-alist
-      '((top . 20) (left . 20) (width . 120) (height . 40)))
+      '((top . 20) (left . 20) (width . 100) (height . 37)))
 
 ;; My ears!
 (setq visible-bell t)
