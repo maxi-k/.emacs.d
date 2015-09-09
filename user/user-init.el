ccc2cdb4eab78adf0d5ed204a9da969d43d6d402
Maxi <maxi.kuschewski@gmail.com>
2015-09-10 01:05:05 +0200

    Add basic functionality to init with org-mode

--- user/user-init.el
+++ user/user-init.el
@@ -1,6 +1,10 @@
 (let ((default-directory "~/.emacs.d/user/"))
   (normal-top-level-add-subdirs-to-load-path))
 ;;;;;; LOADING GENERAL ;;;;;;
+(require 'ob-tangle)
+(setq dotfiles-dir (file-name-directory (or (buffer-file-name) load-file-name)))
+;; load up all literate org-mode files in this directory
+(mapc #'org-babel-load-file (directory-files dotfiles-dir t "\\.org$"))
 
 ;; Loading packages
 (require 'loads)
