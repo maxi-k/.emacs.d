f66d7d8368ecba2223574d06213448b863043e1d
Maxi <maxi.kuschewski@gmail.com>
2015-09-26 14:33:39 +0200

    Removed tromey from package archives

--- init.el
+++ init.el
@@ -8,7 +8,6 @@
 ;; Add the package archives
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
-(add-to-list 'package-archives '("tromey" . "http://tromey.com/elpa/") t)
 
 ;; Initialize the package-management
 (package-initialize)
